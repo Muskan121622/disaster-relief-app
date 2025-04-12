8583ae9e7899b4bd3b0f953c5560d0c690ed82c1
Your Name <muskana1216@gmail.com>
2025-04-12 09:21:46 +0530

    okkkk

--- frontend/src/components/disasterdetails.tsx
+++ frontend/src/components/disasterdetails.tsx
@@ -1330,7 +1330,7 @@ const DisasterDetails = () => {
           </button>
 
           <button
-            onClick={() => navigate("/payment-method")}
+            onClick={() => navigate("/donate")}
             className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
             Financial
